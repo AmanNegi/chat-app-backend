266571f1e676f3f66c2fe1ae443f39d2a55dbc96
amannegi <akuro787898@gmail.com>
2022-12-29 21:36:06 +0530

    Updated CORS policy

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const http = require("http").Server(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "",
+  },
+});
 //   {
 //   cors: {
 //     origin: "http://localhost:3000",
@@ -16,7 +20,12 @@ const { connectDatabase } = require("./helper/db.js");
 const cors = require("cors");
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://amannegi.github.io",
+    methods: "GET,HEAD,PUT,PATCH,DELETE",
+  })
+);
 app.use(express.urlencoded());
 app.use("/chat", chat);
 
